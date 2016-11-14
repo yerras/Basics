a577c09437624a6d3b53ff6235a377df937eda17
yerras <yerras@UK-L57GZ462.groupinfra.com>
2016-11-14 17:14:24 +0000

    Signed-off-by: yerras <yerras@UK-L57GZ462.groupinfra.com>

---
+++ Basics/src/coreJava/Car4Constructor.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package coreJava;
+
+/**
+ * @author yerras
+ *
+ */
+public class Car4Constructor {
+
+	private String sMake;
+	private String sModel;
+	private String sTransmission;
+	String sColour;
+	int iYear;
+	int iDoors;
+	boolean bRightHandDrive;
+	
+	public Car4Constructor(){
+		
+		sMake="BMW";
+		sModel="i8";
+		sTransmission="Auto";
+	}
+	
+	public void DisplayDetails(){
+		System.out.println("Make of the Car is: "+sMake);
+		System.out.println("Model of the Car is: "+sModel);
+		System.out.println("Colour of the Car is: "+sColour);
+		System.out.println("Transmission of the Car is: "+sTransmission);
+		System.out.println("Year of Manufacture is: "+iYear);
+		System.out.println("No of Doors: "+iDoors);
+		System.out.println("Is the Car Right Hand Drive: "+bRightHandDrive);
+	}
+}
